5228a699848982fec0e48971caead062b040ee77
Ian Cowley <7465926+ian-cowley@users.noreply.github.com>
2026-02-13 19:58:58 +0000

    Add AppUserModelID for pinned taskbar icon support

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,16 +1,25 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
 using LinksAndMore.Services;
 
 namespace LinksAndMore;
 
 public partial class App : Application
 {
+    [DllImport("shell32.dll", SetLastError = true)]
+    private static extern void SetCurrentProcessExplicitAppUserModelID(
+        [MarshalAs(UnmanagedType.LPWStr)] string appId);
+
     public static IDataService DataService { get; } = new DataService();
     public static ISecurityService SecurityService { get; } = new SecurityService();
     public static IBiometricService BiometricService { get; } = new BiometricService();
 
     protected override async void OnStartup(StartupEventArgs e)
     {
+        // Must be set before any UI is created so Windows properly
+        // associates the pinned taskbar shortcut with this app's icon
+        SetCurrentProcessExplicitAppUserModelID("IanCowley.LinksAndMore");
+
         base.OnStartup(e);
 
         try
